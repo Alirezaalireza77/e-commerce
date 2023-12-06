0f81ed1e756a19b4927f1ea2e3f0187ff2e09aee
alirezaalireza77 <alireza.hashemian77@gmail.com>
2023-12-06 21:45:01 +0330

    my shop project

---
+++ shop/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+
+def home(request):
+    all_products = Product.objects.all()
+    return render(request, 'index.html', context={'products': all_products})
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def login_user(request, ):
+    if request.method == "POST":
+        username = request.POST('username')
+        password = request.POST('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'با موفقیت وارد شدید')
+            return redirect('home')
+        else:
+            messages.success(request, 'مشکلی در ورود شما وجود دارد')
+    else:
+        return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "با موفقیت خارج شدید!")
+    return redirect("home")
+
+
+def product(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, "product.html", {"product": product})
