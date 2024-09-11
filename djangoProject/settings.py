1f99cd4476da927f39ee3b60c8675b89157dfc77
alireza <alireza.hashemian77@gmail.com>
2024-09-11 12:11:21 +0330

    changing db to postgres

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- shop/models.py
+++ shop/models.py
@@ -39,38 +39,58 @@ class Product(models.Model):
 class Order(models.Model):
     status_choice=[
         ('new', 'New'),
-        ('paid', 'Paid'),
+       ('paid', 'Paid'),
         ('sent', 'Sent'),
-        ('cancel', 'Cancel'),
+       ('cancel', 'Cancel'),
     ]
+    
+    valid_status_changing =[
+        'new': {'paid', 'cancel'},
+        'paid': {'sent'},
+        'sent': {},
+        'cancel':{},
+    ]
+
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1,)
-    status = models.CharField(max_length=20, choices=status_choice, default='new')
+    status = models.CharField(max_length=20, choices=valid_status_changing, default='new')
     name = models.ForeignKey(Customer, on_delete=models.CASCADE)
     address = models.TextField(max_length=1000, blank=False)
     phone = models.CharField(max_length=20, blank=False)
     date = models.DateField(auto_now_add=True)
+
     
     def change_status(self, new_status):
-        if self.status == 'new':
-            if new_status in ['paid', 'cancel']:
-                self.status = new_status
-            else:
-                raise ValidationError('Invalid changing status from new.')
-        elif self.status == 'paid':
-            if self.status == 'sent':
-                self.status = new_status
-            else:
-                raise ValidationError('Invalid changing status from paid level.')
-        elif self.status =='sent':
-            raise ValidationError('your order sent to you and you cannot change status position.')
-        elif self.status =='cancel':
-            raise ValidationError('you cannot change status from cancel.')
+        current_status = self.status
+        if self.status not in self.valid_status_changing.get(current_status, {}):
+            raise ValidationError(f"Invalid status and you cannot change status from {current_status} to {new_status}")
         else:
-            raise ValidationError('unknown status.')
-        self.save()
+            self.status = new_status
+            
+    
+    #def change_status(self, new_status):
+    #    if self.status == 'new':
+    #       if new_status in ['paid', 'cancel']:
+    #            self.status = new_status
+    #        else:
+    #            raise ValidationError('Invalid changing status from new.')
+    #    elif self.status == 'paid':
+    #        if self.status == 'sent':
+    #            self.status = new_status
+    #        else:
+    #            raise ValidationError('Invalid changing status from paid level.')
+    #    elif self.status =='sent':
+    #        raise ValidationError('your order sent to you and you cannot change status position.')
+    #    elif self.status =='cancel':
+    #        raise ValidationError('you cannot change status from cancel.')
+    #    else:
+    #        raise ValidationError('unknown status.')
+    #    self.save()
 
 
+     
+
+    
     def __str__(self):
         return f' order:{self.product} - status: {self.status}'
     
