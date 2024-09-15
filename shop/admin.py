294ac287d09784e0f68db8918760f679f118df82
alireza <alireza.hashemian77@gmail.com>
2024-09-15 13:40:59 +0330

    modifying admin.py by using tabular and adding verbose name to models.

--- shop/admin.py
+++ shop/admin.py
@@ -1,24 +1,61 @@
 from django.contrib import admin
 from .models import Customer, Product, Category, Order, OrderStatusChangeLog
 
-# admin.site.register(Category)
-# admin.site.register(Product)
-# admin.site.register(Customer)
-# admin.site.register(Order)
-# admin.site.register(OrderStatusChangeLog)
+class ProductCategoryInline(admin.TabularInline):
+    model = Product
+    fk_name = "category" 
+    extra = 1
+    fields = ['name','price','star','cast_price','picture']
+
+
+class OrderProductInline(admin.TabularInline):
+    model = Order
+    fk_name = "product"
+    extra = 1
+    fields = ['name','quantity','status','phone','product']
+    exclude = ['date','is_active','address']
+
+
+class OrderChangeInline(admin.TabularInline):
+    model = OrderStatusChangeLog
+    fk_name = "order"
+    extra = 1
+    fields = ['old_status','new_status']
+
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
     list_filter = ['name','id']
     search_fields = ['name']
+    inlines = [ProductCategoryInline]
     fieldsets = (
-        (None, {
-            "fields": ('name','id'),
+        ('Category', {
+            "fields": ('name',),
                }), 
             )
 
     
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'is_sale']
+    list_filter = ['category', 'is_sale'] 
+    search_fields = ['name','id'] 
+    inlines = [OrderProductInline]
+    fieldsets = (
+        ('Product', {
+            "fields": (
+                'name',
+                'description',
+                'category',
+                'price',
+                'cast_price',  
+                'is_sale',
+                'star', 
+            ),
+        }),
+    )
 
 
 
@@ -26,9 +63,9 @@ class CategoryAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['name','lastname','email','phone_number','is_active']
     list_filter = ['phone_number','email']
-    search_fields = ['phone_number']
+    search_fields = ['phone_number','id']
     fieldsets = (
-        (None, {
+        ('Customer', {
             "fields": (
                 'name',
                 'lastname',
@@ -47,32 +84,36 @@ class CustomerAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['product','status','name','phone','is_active']
     list_filter = ['status','name','phone']
-    search_fields = ['id','name']
+    search_fields = ['name']
+    inlines = [OrderChangeInline]
     fieldsets = (
-        (None, {
+        ('Order', {
             "fields": (
                 'product',
-                'quentity',
+                'quantity',
                 'status',
                 'name',
                 'phone',
                 'is_active',
                 'address',
-                'date',
     
             ),
         }),
     )
     
 
-@admin.register(OrderStatusChangeLog)
-class OrderStatusChangeLogAdmin(admin.ModelAdmin):
-    list_display = ['order','old_status','new_status','changed_at']
-    list_filter = ['order']
-    search_fields = ['id','order']
-    fieldsets = (
-        (None, {
-            "fields": ('order','old_status','new_status','changed_at'),
-               }), 
-            )
+# @admin.register(OrderStatusChangeLog)
+# class OrderStatusChangeLogAdmin(admin.ModelAdmin):
+#     list_display = ['order','old_status','new_status','changed_at']
+#     list_filter = ['order']
+#     search_fields = ['order','id']
+#     fieldsets = (
+#         ('LogChanging', {
+#             "fields": ('order',
+#                        'old_status',
+#                        'new_status',
+#                        'changed_at',
+#                     ),
+#                }), 
+#             )
         
--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,10 @@ from datetime import datetime
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
@@ -25,6 +29,10 @@ class Customer(models.Model):
     )
     is_active = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
+
     def __str__(self):
         return f'{self.name}{self.lastname}'
 
@@ -35,7 +43,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(
         max_digits=12, decimal_places=0, null=True, blank=True)
-    picture = models.ImageField(upload_to='upload/product', null=True)
+    picture = models.ImageField(upload_to='upload/product', null=True, blank=True)
     cast_price = models.DecimalField(
         decimal_places=0, max_digits=12, null=True)
     is_sale = models.BooleanField(default=False)
@@ -43,7 +51,11 @@ class Product(models.Model):
         default=0,
         validators=[MinValueValidator(0), MaxValueValidator(5)]
     )
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
+        
     def __str__(self):
         return self.name
 
@@ -72,6 +84,18 @@ class Order(models.Model):
     phone = models.CharField(max_length=20, blank=False)
     date = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Orders'
+        verbose_name_plural = 'Orders'
+
+
+    def clean(self):
+        super().clean()
+        if self.quantity < 1:
+            raise ValidationError(f'quentities of order must be 1 atleast.')
+        if not self.phone.isdigit:
+            raise ValidationError(f'phone number must contain only number.')
 
     @transaction.atomic
     def change_status(self, new_status):
