673b2f6e5215c3e6c335c359ef8855e5810aab6a
alireza <alireza.hashemian77@gmail.com>
2024-09-18 10:53:41 +0330

    add models of Cart and CartItem and develope admin panel for them.

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
-
+from .models import Cart, CartItem
 # Register your models here.
+
+class CartItemCartInline(admin.TabularInline):
+    model = CartItem
+    fk_name = "cart"
+    extra = 1
+    fields = ['cart','product', 'quantity', 'price']
+
+
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['customer','created_at','total_amount']
+    search_fields = ['customer']
+    list_filter = ['id', 'customer']
+    inlines = [CartItemCartInline]
+    fieldsets = (
+        ('Cart', {
+            "fields": (
+                'customer',
+                'total_amount',
+            ),
+        }),
+    )
+    
+    
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart','product','quantity','price']
+    search_fields = ['product', 'cart']
+    list_filter = ['id', 'product']
+    fieldsets = (
+        ('CartItem', {
+            "fields": (
+                'cart',
+                'product',
+                'price',
+            ),
+        }),
+    )
+    
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from customer.models import Customer
+from shop.models import Product
 
 # Create your models here.
+
+class Cart(models.Model):
+    customer = models.OneToOneField(Customer, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    Changed_at = models.DateTimeField(auto_now=True)
+    total_amount = models.DecimalField(max_digits=12, decimal_places=0, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
+
+
+    def calculate_total_price(self):
+        total_price = 0
+        total_quantity = 0
+        cart_items = self.cartitem_set.all()
+
+        for product in cart_items:
+            total_price += product.product.price * product.quantity
+            total_quantity += product.quantity
+
+        self.total_amount = total_price
+        self.save()
+        return self.total_amount, total_price
+
+
+    def __str__(self):
+        return f'{self.customer}'
+    
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=12, decimal_places=0)
+
+    class Meta:
+        verbose_name = 'CartItem'
+        verbose_name_plural = 'CartItems'
+
+
+    def __str__(self):
+        return f'{self.quantity}'
