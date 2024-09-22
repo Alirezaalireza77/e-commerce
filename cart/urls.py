65267c8e3b99c0a196386779bd43e9b3b0cb216d
alireza <alireza.hashemian77@gmail.com>
2024-09-22 14:26:01 +0330

    adding route to urls.py for cart app.

--- cart/urls.py
+++ cart/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import CartItemListViewset, CartItemViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+
+router = DefaultRouter()
+router.register(r'cart-items', CartItemViewSet, basename='cartitem')
+router.register(r'cart-list', CartItemListViewset, basename='cartlist')
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
