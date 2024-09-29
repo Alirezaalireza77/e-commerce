d5e185d8b8488fc51594dad58f07b8c4dab7cb86
Alirezaalireza77 <alireza.hashemian77@gmail.com>
2024-09-29 10:21:27 +0330

    add views and urls of signup.

--- shop/urls.py
+++ shop/urls.py
@@ -7,13 +7,10 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'product-list', ProductList, basename = 'productlist')
-
+router.register(r'signup', SignUpViewSet, basename='signup')
 
 urlpatterns = [
     path('products/', include(router.urls)),
-    # path('about/', views.about, name='about'),
-    # path('api/login/', obtain_auth_token, name='login'),
-    # path('api/logout/', LogoutViewSet.as_view(), name='logout'),
-    # path('api/products/<int:pk>', ProductListViewSet, name='product'),
-    # path('api/signup/', SignUpViewSet.as_view(), name='signup'),
+    path('signup/', include(router.urls))
+
 ]
