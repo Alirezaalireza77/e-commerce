1a54a3fec9f87cd3ce2402f87922606de24e2126
Alirezaalireza77 <alireza.hashemian77@gmail.com>
2024-09-30 11:58:08 +0330

    add urls pattern for order.

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import OrderViewSet
+from rest_framework.routers import DefaultRoutere
 
+router = DefaultRoutere()
+router.register(r'order', OrderViewSet, basename='order')
 urlpatterns = [
-    
+    path('order/', include(router.urls)),
 ]
